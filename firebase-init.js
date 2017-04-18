efa507b9cd04d92a86f3bc740c8901124d9c4654
Pavan Chilukuri <chpavanece@gmail.com>
2017-04-17 22:28:01 -0500

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+
+// Copy your own configuration values here! Generally, You would get that when you create a project in Firebase
+var config = {
+    apiKey: "AIza------------------------------------",
+    authDomain: "---------.firebaseapp.com",
+    databaseURL: "https://----------.firebaseio.com",
+    projectId: "---------------",
+    storageBucket: "------------.appspot.com",
+    messagingSenderId: "xxxxxxxxxxxx"
+};
+firebase.initializeApp(config);
